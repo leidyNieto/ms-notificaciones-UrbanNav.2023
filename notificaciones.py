64979799a49abde85aea0a8cf954d02768045501
AndresRiosCast <Andres.rios31381@ucaldas.edu.co>
2023-11-21 23:58:06 -0500

    Se agrega funcion para enviar factura y para solicitar ayuda cuando se preciona el boton de panico

--- notificaciones.py
+++ notificaciones.py
@@ -134,5 +134,119 @@ def send_pqrs_email():
         print(f'Error: {str(e)}')
         return "<p>Error en el servidor.</p>", 500
     
+@app.route("/factura", methods=['POST'])
+def send_factura_email():
+    try:
+        data = request.get_json()
+        print(f'Solicitud recibida: {data}')  # Imprimir la solicitud recibida
+        nombre = data.get('nombre', '')
+        fecha = data.get('fecha', '')
+        costo = data.get('costo', '')
+        correo = data.get('correo', '') 
+
+        if fecha and costo and nombre and correo:
+            SENDER = "Andres <andresrios774@gmail.com>" 
+            AWS_REGION = "us-east-2"
+            SUBJECT = f"Factura electronica - {nombre} - {fecha}"
+            BODY_TEXT = f"Factura de viaje de {nombre}"
+            BODY_HTML = f"""<html>
+                <head>
+                    <style>
+                        h1 {{
+                            text-align: center;
+                        }}
+                    </style>
+                </head>
+                <body>
+                    <h1>Factura de Viaje</h1>
+                    <p>Nombre: {nombre}</p>
+                    <p>Fecha: {fecha}</p>
+                    <p>Costo: {costo}</p>
+                    <p>Correo: {correo}</p>
+                </body>
+            </html>"""
+
+            CHARSET = "UTF-8"
+
+            client = boto3.client(
+                "ses",
+                aws_access_key_id=config("AWS_API_KEY"),
+                aws_secret_access_key=config("AWS_SECRET_KEY"),
+                region_name=AWS_REGION
+            )
+            response = client.send_email(
+                Destination={'ToAddresses': [correo]},
+                Message={
+                    'Body': {
+                        'Html': {'Charset': CHARSET, 'Data': BODY_HTML},
+                        'Text': {'Charset': CHARSET, 'Data': BODY_TEXT},
+                    },
+                    'Subject': {'Charset': CHARSET, 'Data': SUBJECT},
+                },
+                Source=SENDER,
+            )
+            return f"<p>Correo electrónico enviado a {correo}! ID del mensaje: {response['MessageId']}</p>"
+        else:
+            return "<p>Parámetros faltantes en la solicitud.</p>", 400
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        return "<p>Error en el servidor.</p>", 500
+    
+
+@app.route("/panico", methods=['POST'])
+def send_boton_panico():
+    try:
+        data = request.get_json()
+        print(f'Solicitud recibida: {data}')  # Imprimir la solicitud recibida
+        ruta = data.get('ruta', '')
+        datos_conductor = data.get('datos_conductor', '')
+        datos_usuario = data.get('datos_usuario', '')
+        correo_ayuda = data.get('correo_ayuda', '') 
+
+        if datos_conductor and datos_usuario and ruta and correo_ayuda:
+            SENDER = "Andres <andresrios774@gmail.com>" 
+            AWS_REGION = "us-east-2"
+            SUBJECT = f"Alguien preciono el boton de panaico"
+            BODY_TEXT = f"Factura de viaje de "
+            BODY_HTML = f"""<html>
+                <head>
+                    <style>
+                        h1 {{
+                            text-align: center;
+                        }}
+                    </style>
+                </head>
+                <body>
+                    <h1>Alguien necesita tu ayuda</h1>
+                    <p>el usuario ${datos_usuario} esta teniendo problemas en las ruta ${ruta} que esta a cargo del conductor ${datos_conductor}</p>
+                </body>
+            </html>"""
+
+            CHARSET = "UTF-8"
+
+            client = boto3.client(
+                "ses",
+                aws_access_key_id=config("AWS_API_KEY"),
+                aws_secret_access_key=config("AWS_SECRET_KEY"),
+                region_name=AWS_REGION
+            )
+            response = client.send_email(
+                Destination={'ToAddresses': [correo_ayuda]},
+                Message={
+                    'Body': {
+                        'Html': {'Charset': CHARSET, 'Data': BODY_HTML},
+                        'Text': {'Charset': CHARSET, 'Data': BODY_TEXT},
+                    },
+                    'Subject': {'Charset': CHARSET, 'Data': SUBJECT},
+                },
+                Source=SENDER,
+            )
+            return f"<p>Correo electrónico enviado a {correo_ayuda}! ID del mensaje: {response['MessageId']}</p>"
+        else:
+            return "<p>Parámetros faltantes en la solicitud.</p>", 400
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        return "<p>Error en el servidor.</p>", 500
+
 if __name__ == "__main__":
     app.run()
